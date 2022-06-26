d818a5b0dab04f32b70ffbcc5f0f782872165f67
mohitraj2741 <mohitraj2741@gmail.com>
2022-06-26 17:17:14 +0530

    updated with LOG, removal of .yaml and comments

--- ZAPscan.py
+++ ZAPscan.py
@@ -13,18 +13,21 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-
+#getting current working directory path
 cwd_path = os.getcwd()
 
-
+#getting target site input
 target_site = input("Enter Target Site: ")
 
 def main():
+    #storing the command
     cmd = f'docker run -v {cwd_path}:/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t {target_site} -r testreport.html'
     proc_cmd = cmd.split(' ')
 
+    #running subprocess
     process = subprocess.Popen(proc_cmd, stdout=subprocess.PIPE)
 
+    #printing the output from stdout and stop as soon as process ends
     for line in iter(process.stdout.readline, ""):
         line = line.decode()
         proc_running = process.poll() is None
@@ -33,21 +36,22 @@ def main():
 
         print("[ZAP]", line[:-1])
 
-
+    #checking for presence of testreport
     if os.path.exists('testreport.html'):
         print("[LOG] Test report generated successfully")
     else:
         print("[ERR] Test report generation failed")
 
+    #getting the source address from .env
     fromaddr = os.getenv("FROMADDR")
 
+    #getting applicaton specific password of source address
     email_pass= getpass.getpass(prompt='Enter password ')
     
+    #input destination address
     toaddr =input("Enter Destination email address: ")
 
     
-    
-    #email_pass = "ihna lsxk blyz lini"
 
     # instance of MIMEMultipart
     msg = MIMEMultipart()
@@ -107,15 +111,25 @@ def main():
     
     print("[LOG] Mail sent")
 
+    #close the attachment
     attachment.close()
 
+    #removing reports from the os
     os.remove('testreport.html')
+    os.remove('zap.yaml')
 
-    time.sleep(3)
+    time.sleep(2)
 
+    #removing docker exited docker containers
     cmd2 = f'docker container prune -f'
+
     proc_cmd2 = cmd2.split(' ')
 
     subprocess.Popen(proc_cmd2, stdout=subprocess.PIPE)
+
+    print("[LOG] Destroyed Exited containers")
+
+    time.sleep(2)
+
 if __name__ == "__main__":
     main()
